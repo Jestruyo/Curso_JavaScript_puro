c088633d86e56f9dcbab7d6363c50d2da9b287cb
Jestruyo <jesusdavidtrujilloteheran@gmail.com>
2025-01-17 18:01:30 -0500

    ADD: todoListUL

--- 04_Todo_app/src/todos/app.js
+++ 04_Todo_app/src/todos/app.js
@@ -31,15 +31,48 @@ export const App = (elementId) => {
 
     // Referencias a elementos HTML
     const newDescriptionInput = document.querySelector(elementIDs.NewTodoImput);
+    const todoListUL = document.querySelector(elementIDs.TodoList);
 
-    // Listener
+    // Listener de la App
+
+    /**
+     * Evento para agregar un nuevo todo
+     */
     newDescriptionInput.addEventListener('keyup', (event) => {
-        console.log(event);
+        // console.log(event);
         if(event.keyCode !== 13) return;
         if(event.target.value.trim().length === 0) return;
         todoStore.addTodo(event.target.value,);
         DisplayTodos();
         event.target.value = '';
+        
+    });
+
+    /**
+     * Evento para marcar un todo como completado
+     */
+    todoListUL.addEventListener('click', (event) => {
+        // console.log(event.target);
+        const element = event.target.closest('[data-id]');
+        todoStore.toggleTodo(
+            element.getAttribute('data-id')
+        );
+        DisplayTodos();
+
+    });
+
+
+    /**
+     * Evento para eliminar un todo
+     */
+    todoListUL.addEventListener('click', (event) => {
+        const isDestroyElement = event.target.className === 'destroy';
+        // console.log({isDestroyElement});
+        if(!isDestroyElement) return;
+        todoStore.deleteTodo(
+            event.target.closest('[data-id]').getAttribute('data-id')
+        );
+        DisplayTodos();
         
     });
 }
